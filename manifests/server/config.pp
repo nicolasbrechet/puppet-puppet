c47d74deca357996eb1ebc9b1476db6e53c4f737
nicolasbrechet <nicolasbrechet@users.noreply.github.com>
2014-07-04 10:29:15 +0200

    Update config.pp

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -121,7 +121,7 @@ class puppet::server::config {
   if $puppet::server::autosign {
     ini_setting { 'autosign':
         setting => 'autosign',
-        value   =>  $puppet::server::dns_alt_names
+        value   =>  $puppet::server::autosign
     }
   }
 
